7c86f5dd7ff8546d4522237c217e1a2b940a4de2
oleleire <oleleire1@gmail.com>
2023-10-05 15:30:11 +0200

    ENDRET KODEN

--- Servo.ino
+++ Servo.ino
@@ -1,24 +1,28 @@
 #include <Servo.h>
 
 const int servoPin = 9; 
-const int t = 1000;
+const int t = 6000;
+int pos = 0; 
 
+Servo servo; 
 
-Servo servo 
-
-void setup(){
+void setup() {
 Serial.begin(9600);
 servo.attach(servoPin);
 }
 
 
-void loop(){
-    servo.write(0);
-    delay(t);
-    servo.write(90);
-    delay(t);
-    servo write(180);
-    delay(t);
+void loop() {
+    
+    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    servo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(500);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    servo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
 }
 
 
